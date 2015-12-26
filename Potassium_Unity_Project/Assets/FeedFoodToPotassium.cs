5a19d0b5da97f3e9e0ec67d1618f31c201e49300
xuechi.li <lixuechi@hotmail.com>
2015-12-26 18:36:03 +0800

    * split move direction and move pattern + move in a square * abstract methods for move horizontally & vertically

---
+++ Potassium_Unity_Project/Assets/FeedFoodToPotassium.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FeedFoodToPotassium : MonoBehaviour {
+
+	void Start () {
+	
+	}
+
+	void Update () {
+
+		if (Input.GetKeyUp (KeyCode.Space)) 
+		{
+			Debug.Log("Cast a piece of food");
+
+		}
+	
+	}
+}
--- Potassium_Unity_Project/Assets/PotassiumMove.cs
+++ Potassium_Unity_Project/Assets/PotassiumMove.cs
@@ -10,15 +10,20 @@ public class PotassiumMove : MonoBehaviour {
 	const int MOVE_DOWN = 1;
 	const int MOVE_LEFT = 2;
 	const int MOVE_RIGHT = 3;
+	const int MOVE_IN_CIRCLE = 4;
+	const int MOVE_IN_SQUARE = 5;
+	const int STAY_STATIC = 6;
+	int NUM_OF_MOVE_OPTIONS = 0;
 
-	private int randomMoveDirection = 0;
+	private int randomMovePattern = 0;
 	private int currMoveDirection = 0;
+	private int currMovePattern = 0;
 
 	private Vector3 horizontalMoveUnit = new Vector3(0.01f, 0, 0);
 	private Vector3 verticalMoveUnit = new Vector3(0, 0.01f, 0);
 
 	void Start () {
-	
+		NUM_OF_MOVE_OPTIONS = 6;
 	}
 
 	void Update () {
@@ -29,23 +34,36 @@ public class PotassiumMove : MonoBehaviour {
 			Debug.Log("Last time checkpoint updated");
 
 			// determine where to go in each a few seconds
-			randomMoveDirection = Random.Range(MOVE_UP, MOVE_RIGHT + 1);
-			switch(randomMoveDirection)
+			randomMovePattern = Random.Range(MOVE_UP, NUM_OF_MOVE_OPTIONS + 1);
+			switch(randomMovePattern)
 			{
 			case MOVE_UP: Debug.Log("Move up");
 						  currMoveDirection = MOVE_UP;
+				currMovePattern = MOVE_UP;
 						  break;
 			case MOVE_DOWN: Debug.Log("Move down");
 						    currMoveDirection = MOVE_DOWN;
+				currMovePattern = MOVE_DOWN;
 							break;
 			case MOVE_LEFT: Debug.Log("Move left");
 							currMoveDirection = MOVE_LEFT;
+				currMovePattern = MOVE_LEFT;
 							break;
 			case MOVE_RIGHT: Debug.Log("Move right");
 							 currMoveDirection = MOVE_RIGHT;
+				currMovePattern = MOVE_RIGHT;
 							 break;
+			case MOVE_IN_CIRCLE: Debug.Log("Move in a circle");
+				currMoveDirection = STAY_STATIC;
+				currMovePattern = MOVE_IN_CIRCLE;
+				break;
+			case MOVE_IN_SQUARE: Debug.Log("Move in a square");
+				currMoveDirection = STAY_STATIC;
+				currMovePattern = MOVE_IN_SQUARE;
+				break;
 			default: Debug.Log("Default move up");
 					 currMoveDirection = MOVE_UP;
+				currMovePattern = MOVE_UP;
 					 break;
 			}
 
@@ -56,7 +74,7 @@ public class PotassiumMove : MonoBehaviour {
 		// get the current position of Potassium
 		currentPosition = transform.position;
 
-		switch(currMoveDirection)
+		switch(currMovePattern)
 		{
 		case MOVE_UP: currentPosition += verticalMoveUnit;
 			break;
@@ -66,6 +84,10 @@ public class PotassiumMove : MonoBehaviour {
 			break;
 		case MOVE_RIGHT: currentPosition += horizontalMoveUnit;
 			break;
+		case MOVE_IN_CIRCLE: moveInACircle();
+			break;
+		case MOVE_IN_SQUARE: currentPosition = moveInASquare();
+			break;
 		}
 
 
@@ -74,15 +96,77 @@ public class PotassiumMove : MonoBehaviour {
 		{
 			Debug.Log("Move direction changed due to the boundary");
 			currMoveDirection = (currMoveDirection == MOVE_LEFT) ? MOVE_RIGHT : MOVE_LEFT;
+			currMovePattern = (currMovePattern == MOVE_LEFT) ? MOVE_RIGHT : MOVE_LEFT;
 		}
 		if (currentPosition.y <= -4 || currentPosition.y >= 4) 
 		{
 			Debug.Log("Move direction changed due to the boundary");
 			currMoveDirection = (currMoveDirection == MOVE_UP) ? MOVE_DOWN : MOVE_UP;
+			currMovePattern = (currMovePattern == MOVE_UP) ? MOVE_DOWN : MOVE_UP;
 		}
 
 		
 		// copy the changes made for current position
 		transform.position = currentPosition;
 	}
+
+	Vector3 moveHorizontally(Vector3 currPos, bool isToLeft, int speed)
+	{
+		currPos += isToLeft ? (- horizontalMoveUnit * speed) : (horizontalMoveUnit * speed);
+		return currPos;
+	}
+
+	Vector3 moveVertically(Vector3 currPos, bool isToUp, int speed)
+	{
+		currPos += isToUp ? (- verticalMoveUnit * speed) : (verticalMoveUnit * speed);
+		return currPos;
+	}
+
+	// Counter-clockwise: left -> down -> right -> up -> left
+	static float timeSinceStartMovingInSquare = 0;
+	static Vector3 currPosWhenMovingInSquare = new Vector3();
+	Vector3 moveInASquare()
+	{
+
+		Debug.Log ("func moveInASquare called");
+		Debug.Log ("timeSinceStartMovingInSquare == " + timeSinceStartMovingInSquare);
+		if (timeSinceStartMovingInSquare <= 2) 
+		{
+			currPosWhenMovingInSquare = moveHorizontally(currPosWhenMovingInSquare, true, 2);
+			currMoveDirection = MOVE_LEFT;
+			Debug.Log("move in a square: left");
+		} 
+		else if (timeSinceStartMovingInSquare <= 4) 
+		{
+			currPosWhenMovingInSquare = moveVertically(currPosWhenMovingInSquare, false, 2);
+			currMoveDirection = MOVE_DOWN;
+			Debug.Log("move in a square: down");
+		} 
+		else if (timeSinceStartMovingInSquare <= 6) 
+		{
+			currPosWhenMovingInSquare = moveHorizontally(currPosWhenMovingInSquare, false, 2);
+			currMoveDirection = MOVE_RIGHT;
+			Debug.Log("move in a square: right");
+		} 
+		else if (timeSinceStartMovingInSquare <= 8) 
+		{
+			currPosWhenMovingInSquare = moveVertically(currPosWhenMovingInSquare, true, 2);
+			currMoveDirection = MOVE_UP;
+			Debug.Log("move in a square: up");
+		} 
+		else 
+		{
+
+		}
+		timeSinceStartMovingInSquare += Time.deltaTime;
+
+		return currPosWhenMovingInSquare;
+	}
+
+	const int moveCircleRadius = 1;
+	void moveInACircle()
+	{
+		Vector3 originOfCircle = transform.position - new Vector3 (0, moveCircleRadius, 0);
+	}
+
 }
