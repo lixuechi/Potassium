b2cd293e379134f51f625315649587c8ca13764a
xuechi.li <lixuechi@hotmail.com>
2015-12-27 19:05:20 +0800

    + def HP * make Global Update script attached for handling global updates

---
+++ Potassium_Unity_Project/Assets/PotassiumStatus.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PotassiumStatus : GlobalPotassium {
+
+	public GameObject hpTextObj;
+	Text hpText;
+
+	void Start () {
+		hpText = hpTextObj.GetComponent<Text>();
+	}
+	
+	void Update () {
+	
+		if(hpText != null)
+		{
+			hpText.text = "HP: " + hpValue;
+			Debug.Log("HP is " + hpValue);
+		}
+
+	}
+}
--- Potassium_Unity_Project/Assets/scripts/GlobalPotassium.cs
+++ Potassium_Unity_Project/Assets/scripts/GlobalPotassium.cs
@@ -6,12 +6,17 @@ public class GlobalPotassium : MonoBehaviour {
 	public static bool isOrangePresent = false;
 	public static Transform orangeTransform;
 	public static bool isTired = true;
+	public static int hpValue = 100;
+	private static float hpValueFloat = 100f;
 
 	void Start () {
 	
 	}
 	
 	void Update () {
-	
+		
+		hpValueFloat -= 0.005f;
+		hpValue = (int)hpValueFloat; 
+
 	}
 }
