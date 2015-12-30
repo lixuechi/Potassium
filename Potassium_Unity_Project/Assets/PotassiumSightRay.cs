b9b644d27039b8613deed241826dfd3c1209a3ad
xuechi.li <lixuechi@hotmail.com>
2015-12-30 22:54:55 +0800

    + sight rays become shorter while hitting colliders (TODO: a bunch of bugs awaiting..)

--- Potassium_Unity_Project/Assets/PotassiumSightRay.cs
+++ Potassium_Unity_Project/Assets/PotassiumSightRay.cs
@@ -4,14 +4,28 @@ using System.Collections;
 public class PotassiumSightRay : MonoBehaviour {
 
 	public Transform sightRayPrefab;
+	Transform[] sightRays = new Transform[36];
 
 	void Start () {
 
-		Transform s = Instantiate(sightRayPrefab, new Vector3(0, 0, 0), Quaternion.identity) as Transform;
+		for(int i = 0; i < 36; i++)
+		{
+			Transform s = Instantiate(sightRayPrefab, this.transform.position, Quaternion.identity) as Transform;
+			s.Rotate(new Vector3(0, 0, i * 10));
+
+			sightRays[i] = s;
+		}
 	
 	}
 	
 	void Update () {
+
+		for(int k = 0; k < 36; k++)
+		{
+			sightRays[k].transform.position = this.transform.position;
+		}
 	
 	}
+
+
 }
---
+++ Potassium_Unity_Project/Assets/SightRayControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SightRayControl : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+		//this.transform.localScale += new Vector3(0.1f * Time.deltaTime, 0, 0);
+
+	}
+
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if(coll.gameObject.tag != "Player")
+		{
+			Debug.Log("Sight ray hits " + coll.gameObject.tag);
+			this.transform.localScale = new Vector3(this.transform.localScale.x * 0.95f, 0.3f, 1);
+		}
+	}
+	/*
+	void OnTriggerStay2D(Collider2D coll)
+	{
+		if(coll.gameObject.tag != "Player")
+		{
+			Debug.Log("Sight ray keeps hitting " + coll.gameObject.tag);
+			this.transform.localScale = new Vector3(this.transform.localScale.x * 0.95f, 0.3f, 1);
+		}
+	}
+	*/
+}
