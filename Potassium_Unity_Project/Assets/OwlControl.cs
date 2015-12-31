8662df5aa6981dd918b37132cb8701b49d195a1b
xuechi.li <lixuechi@hotmail.com>
2015-12-31 21:22:28 +0800

    + harm text when Owl hits Potassium

--- Potassium_Unity_Project/Assets/OwlControl.cs
+++ Potassium_Unity_Project/Assets/OwlControl.cs
@@ -1,31 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class OwlControl : MonoBehaviour {
 
-	void Start () {
-	
+	public Transform harmPrefab;
+	Transform harm;
+	Text harmText;
+	static bool isShowHarmText = false;
+
+	void Start () 
+	{
+		if(harmPrefab != null)
+		{
+			harmText = harmPrefab.gameObject.GetComponent<Text>();
+		}	
 	}
 	
-	void Update () {
+	void Update () 
+	{
 
-			if(Input.GetKey(KeyCode.UpArrow))
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			Debug.Log("move the owl upward");
+			this.transform.position += new Vector3(0, 0.1f, 0);
+		}	
+		else if (Input.GetKey(KeyCode.DownArrow))
+		{
+			this.transform.position += new Vector3(0, -0.1f, 0);
+		}
+		else if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			this.transform.position += new Vector3(-0.1f, 0, 0);
+		}
+		else if(Input.GetKey(KeyCode.RightArrow))
+		{
+			this.transform.position += new Vector3(0.1f, 0, 0);
+		}
+
+		if(isShowHarmText)
+		{
+			if(harm == null)
 			{
-				Debug.Log("move the owl upward");
-				this.transform.position += new Vector3(0, 0.1f, 0);
+				harm = Instantiate(harmPrefab, this.transform.position, Quaternion.identity) as Transform;
 			}
-			else if (Input.GetKey(KeyCode.DownArrow))
-			{
-				this.transform.position += new Vector3(0, -0.1f, 0);
-			}
-			else if(Input.GetKey(KeyCode.LeftArrow))
-			{
-				this.transform.position += new Vector3(-0.1f, 0, 0);
-			}
-			else if(Input.GetKey(KeyCode.RightArrow))
-			{
-				this.transform.position += new Vector3(0.1f, 0, 0);
-			}
+		}
 	
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if(coll.gameObject.tag == "Player")
+		{
+			isShowHarmText = true;
+		}
 	}
 }
