3d94d498aa99748b9c01a373c6998e97e8af467a
xuechi.li <lixuechi@hotmail.com>
2016-01-02 19:41:23 +0800

    + Potassium -> OpenResult -> a bunch of oranges fall down

--- Potassium_Unity_Project/Assets/ButtonsControlForScene0001.cs
+++ Potassium_Unity_Project/Assets/ButtonsControlForScene0001.cs
@@ -6,20 +6,34 @@ using UnityEngine.UI;
 
 public class ButtonsControlForScene0001 : MonoBehaviour {
 
-	void Start () 
+	public GameObject PotassiumIconButton;
+	public GameObject OpenResultButton;
+	public GameObject Os;
+
+	void Awake()
 	{
 		List<string> btnNames = new List<string> ();
 		btnNames.Add ("GoToPlayground");
-
+		btnNames.Add("Fortune");
+		btnNames.Add("OpenResult");
+		
 		foreach (string btnName in btnNames) 
 		{
 			GameObject btnObj = GameObject.Find (btnName);
 			Button btn = btnObj.GetComponent<Button>();
 			btn.onClick.AddListener(delegate()
-			{
+			                        {
 				this.OnClick(btnObj);
 			});
 		}
+
+		OpenResultButton.SetActive (false);
+		Os.SetActive (false);
+	}
+
+	void Start () 
+	{
+
 	}
 	
 	void Update () 
@@ -34,6 +48,17 @@ public class ButtonsControlForScene0001 : MonoBehaviour {
 			case "GoToPlayground":
 			Debug.Log("GoToPlayground button pushed");
 			Application.LoadLevel("0000");
+			break;
+			case "Fortune":
+			Debug.Log("Potassium icon button pushed");
+			OpenResultButton.SetActive(true);
+			PotassiumIconButton.SetActive(false);
+		    break;
+		case "OpenResult":
+			Debug.Log("Open Result button pushed");
+			Os.SetActive(true);
+			OpenResultButton.SetActive(false);
+			PotassiumIconButton.SetActive(true);
 			break;
 		}
 	}
