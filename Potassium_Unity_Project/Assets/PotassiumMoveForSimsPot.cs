935cc8c9b8f1a6c864c8bca70a8bcbb7f9f4c8a7
xuechi.li <lixuechi@hotmail.com>
2016-01-03 19:57:43 +0800

    - revert Potassium to a static sprite from a button

---
+++ Potassium_Unity_Project/Assets/PotassiumMoveForSimsPot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PotassiumMoveForSimsPot : MonoBehaviour {
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.UpArrow)) 
+		{
+
+		}
+	}
+}
