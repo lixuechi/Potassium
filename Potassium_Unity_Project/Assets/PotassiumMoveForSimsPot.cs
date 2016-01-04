793bf45630b4fa64f649854e2ae02e8382e7eb67
xuechi.li <lixuechi@hotmail.com>
2016-01-04 20:26:50 +0800

    + map actions to left-lower button

--- Potassium_Unity_Project/Assets/PotassiumMoveForSimsPot.cs
+++ Potassium_Unity_Project/Assets/PotassiumMoveForSimsPot.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PotassiumMoveForSimsPot : MonoBehaviour {
+
+	public Text reactionText;
+	const int ACTION_VOID = 0;
+	const int ACTION_SLEEP = 1;
+	int currAction = 0;
 
 	void Start () 
 	{
@@ -24,5 +30,30 @@ public class PotassiumMoveForSimsPot : MonoBehaviour {
 		{
 			this.transform.position -= new Vector3(0.1f, 0, 0);
 		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if (coll.gameObject.tag == "Bed") 
+		{
+			reactionText.text = "Sleep";
+			currAction = ACTION_SLEEP;
+		} 
+		else if (coll.gameObject.tag == "Toilet") 
+		{
+			reactionText.text = "Pee/Poop";
+
+		}
+		else if (coll.gameObject.tag == "Bathtub") 
+		{
+			reactionText.text = "Bathe";
+			
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D coll)
+	{
+		reactionText.text = "";
+		currAction = ACTION_VOID;
 	}
 }
