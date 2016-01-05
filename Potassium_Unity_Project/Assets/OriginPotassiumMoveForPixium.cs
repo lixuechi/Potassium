b2e35a8105b7f6713c3c600c851f86f492dcdde7
xuechi.li <lixuechi@hotmail.com>
2016-01-05 21:45:03 +0800

    + origin Potassium move script for Pixium Scene

---
+++ Potassium_Unity_Project/Assets/OriginPotassiumMoveForPixium.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OriginPotassiumMoveForPixium : MonoBehaviour {
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow)) 
+		{
+			this.transform.position += new Vector3(0, 0.1f, 0);
+		}
+		else if (Input.GetKey (KeyCode.DownArrow)) 
+		{
+			this.transform.position += new Vector3(0, -0.1f, 0);
+		}
+		else if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			this.transform.position += new Vector3(-0.1f, 0, 0);
+		}
+		else if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			this.transform.position += new Vector3(0.1f, 0, 0);
+		}
+	
+	}
+}
