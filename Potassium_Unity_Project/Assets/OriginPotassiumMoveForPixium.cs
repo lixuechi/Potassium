c08229d4a499a7060bef54a640d1d6e1c08dd153
xuechi.li <lixuechi@hotmail.com>
2016-01-06 21:58:13 +0800

    + regenerate oranges each time when Potassium moves #TODO add content for above method

--- Potassium_Unity_Project/Assets/OriginPotassiumMoveForPixium.cs
+++ Potassium_Unity_Project/Assets/OriginPotassiumMoveForPixium.cs
@@ -3,27 +3,50 @@ using System.Collections;
 
 public class OriginPotassiumMoveForPixium : MonoBehaviour {
 
+	static bool isRegenerateOranges = false;
+	public Transform OrangePrefab;
+
 	void Start () {
 	
 	}
 
 	void Update () {
-		if (Input.GetKey (KeyCode.UpArrow)) 
+
+		if (Input.GetKey (KeyCode.UpArrow)
+			|| Input.GetKey (KeyCode.DownArrow)
+			|| Input.GetKey (KeyCode.LeftArrow)
+			|| Input.GetKey (KeyCode.RightArrow)) 
 		{
-			this.transform.position += new Vector3(0, 0.1f, 0);
+			if (Input.GetKey (KeyCode.UpArrow)) 
+			{
+				this.transform.position += new Vector3(0, 0.1f, 0);
+			}
+			else if (Input.GetKey (KeyCode.DownArrow)) 
+			{
+				this.transform.position += new Vector3(0, -0.1f, 0);
+			}
+			else if (Input.GetKey (KeyCode.LeftArrow)) 
+			{
+				this.transform.position += new Vector3(-0.1f, 0, 0);
+			}
+			else if (Input.GetKey (KeyCode.RightArrow)) 
+			{
+				this.transform.position += new Vector3(0.1f, 0, 0);
+			}
+
+			// if Potassium moves, regenerate all the oranges
+			isRegenerateOranges = true;
 		}
-		else if (Input.GetKey (KeyCode.DownArrow)) 
+
+		if (isRegenerateOranges) 
 		{
-			this.transform.position += new Vector3(0, -0.1f, 0);
-		}
-		else if (Input.GetKey (KeyCode.LeftArrow)) 
-		{
-			this.transform.position += new Vector3(-0.1f, 0, 0);
-		}
-		else if (Input.GetKey (KeyCode.RightArrow)) 
-		{
-			this.transform.position += new Vector3(0.1f, 0, 0);
+			regenerateOranges();
+			isRegenerateOranges = false;
 		}
 	
+	}
+
+	void regenerateOranges()
+	{
 	}
 }
