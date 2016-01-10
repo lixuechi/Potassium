c16cebb9806a965bf4a185dad7f9fc3ca6f57777
xuechi.li <lixuechi@hotmail.com>
2016-01-10 21:13:02 +0800

    + Potassium Thinking Entity for its free thinking

---
+++ Potassium_Unity_Project/Assets/PotassiumThinksControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PotassiumThinksControl {
+
+	string name = "Potassium";
+	int age = 0;
+	byte[] birthDate = {2016, 1, 10};
+	string gender = "female";
+	string astrologySign = "Capricornus";
+
+	// Constructor
+	PotassiumThinksControl()
+	{
+
+	}
+
+	string getName()
+	{
+		return name;
+	}
+	void setName(string newName)
+	{
+		name = newName;
+	}
+
+	int getAge()
+	{
+		return age;
+	}
+	void setAge(int newAge)
+	{
+		age = newAge;
+	}
+}
