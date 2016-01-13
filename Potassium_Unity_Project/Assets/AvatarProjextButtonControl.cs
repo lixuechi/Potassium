5ad0e1d1a41981a640c8c896854a3483d01ad2c1
xuechi.li <lixuechi@hotmail.com>
2016-01-13 20:25:48 +0800

    + new scene for Potassium’s learning

---
+++ Potassium_Unity_Project/Assets/AvatarProjextButtonControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class AvatarProjextButtonControl : MonoBehaviour {
+
+	void Awake(){
+		List<string> btnNames = new List<string> ();
+		btnNames.Add ("AvataxButton");
+		
+		foreach (string btnName in btnNames) 
+		{
+			GameObject btnObj = GameObject.Find (btnName);
+			Button btn = btnObj.GetComponent<Button>();
+			btn.onClick.AddListener(delegate()
+			                        {
+				this.OnClick(btnObj);
+			});
+		}
+	}
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	void OnClick(GameObject sender)
+	{
+		switch (sender.name) {
+			case "AvataxButton":
+				Debug.Log ("Avatax button pushed");
+				break;
+		}
+	}
+}
